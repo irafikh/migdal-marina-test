a12a1234a006e5c652f5f5a1ffbdc1fc2e9080e1
irafikh <74657207+irafikh@users.noreply.github.com>
2022-02-07 16:15:01 +0200

    Create contact-person.model.ts

---
+++ src/app/models/contact-person.model.ts
@@ -0,0 +1,10 @@
+export interface ContactPerson {
+    id: number,
+    deliveryFlag: boolean,
+    name: string,
+    typeCode: number,
+    typeValue: string,
+    address: string,
+    phoneNumber: number,
+    email: string//) {}
+}
